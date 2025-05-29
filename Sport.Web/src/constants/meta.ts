f7678a7d39d8876724ca47b661f0c1519b516ec4
Daria Reu <dominica.diem@gmail.com>
2025-05-29 20:35:56 +0300

    a lot of files. this is why the full isn't in my thesis.

---
+++ Sport.Web/src/constants/meta.ts
@@ -0,0 +1,2 @@
+export const SITE_DESCRIPTION =
+  "Elevate your sports commentary with Power UP. We provide the tools and community for sports writers and fans to publish their articles and connect with a dedicated audience.";
