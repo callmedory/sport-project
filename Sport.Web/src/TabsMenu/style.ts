b498690979cc3aee04bd171d53cb6a3d590fc10f
dory <dominica.diem@gmail.com>
2025-05-29 20:31:33 +0300

    even more src files

---
+++ Sport.Web/src/TabsMenu/style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const TabsMenuWrapper = styled.div`
+  display: flex;
+  margin-bottom: 12px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const FilterTabsWrapper = styled.div(
+  ({ theme }) => `
+  display: flex;
+  gap: 6px;
+  ${theme.media.desktop}{
+      gap: 12px;
+    }
+    
+  .MuiButton-root.MuiButtonBase-root {
+    font-size: 12px;
+    width: 100px;
+    
+    ${theme.media.desktop}{
+        width: 140px;
+        font-size: 15px;
+    }
+  }
+`
+);
+
+interface IStyledTabProps {
+  $isActive: boolean;
+}
+
+export const StyledTab = styled.div<IStyledTabProps>(
+  ({ $isActive }) => `
+  font-weight: ${$isActive ? 800 : 500};
+`
+);
